5ba6208e858858bed15db61d22dac16cbcb19ae9
akhil336 <a.m.akhilesh336@gmail.com>
2024-02-25 17:40:51 +0530

    Added Food Category Cards

--- App.js
+++ App.js
@@ -16,6 +16,48 @@ import ReactDOM from "react-dom/client";
  *  -[Copywrite]
  */
 
+const foodCategoryList=[
+    {
+        id:1,
+        image:"https://imgmediagumlet.lbb.in/media/2020/05/5ebb96964eeff5e29ab651b5_1589352086008.jpg",
+        categoryName:"All-in-1 Meals"
+    },
+    {
+        id:2,
+        image:"https://lh3.googleusercontent.com/4rYxv727Li0_vhwD8QygNoSX1QH8tyXJ3JFBotvYkFthd_PObq4bEvqTeIOvMJTimK7xs9jANPXM4xPF6bLJaywJSD5oYbbaWyTNDOFAvw=w512-rw",
+        categoryName:"Mini Meals"
+    },
+    {
+        id:3,
+        image:"https://www.msrmarket.in/cdn/shop/products/Egg-Curry.jpg",
+        categoryName:"Desi Box"
+    },
+    {
+        id:4,
+        image:"https://static.toiimg.com/thumb/54308405.cms?imgsize=510571&width=800&height=800",
+        categoryName:"Dum Biriyani"
+    },
+    {
+        id:5,
+        image:"https://lh3.googleusercontent.com/4rYxv727Li0_vhwD8QygNoSX1QH8tyXJ3JFBotvYkFthd_PObq4bEvqTeIOvMJTimK7xs9jANPXM4xPF6bLJaywJSD5oYbbaWyTNDOFAvw=w512-rw",
+        categoryName:"Mini Meals"
+    },
+    {
+        id:6,
+        image:"https://www.msrmarket.in/cdn/shop/products/Egg-Curry.jpg",
+        categoryName:"Desi Box"
+    },{
+        id:7,
+        image:"https://lh3.googleusercontent.com/4rYxv727Li0_vhwD8QygNoSX1QH8tyXJ3JFBotvYkFthd_PObq4bEvqTeIOvMJTimK7xs9jANPXM4xPF6bLJaywJSD5oYbbaWyTNDOFAvw=w512-rw",
+        categoryName:"Mini Meals"
+    },
+    {
+        id:8,
+        image:"https://www.msrmarket.in/cdn/shop/products/Egg-Curry.jpg",
+        categoryName:"Desi Box"
+    }
+]
+
 const HeaderComponent = () => {
     return (
         <div className="header">
@@ -28,7 +70,7 @@ const HeaderComponent = () => {
                 </div>
                 <div className="searchBar">
                     <input type="text" name="search" placeholder="Search"></input>
-                    <button id="search" value={SVGCircleElement}></button>
+                    <button id="search">Search</button>
                 </div>
 
             </div>
@@ -40,6 +82,17 @@ const HeaderComponent = () => {
     );
 }
 
+const CategoryCardComponent = (props) => {
+   
+    const {foodCategory}=props;
+    const {id,image,categoryName}=foodCategory;
+    return (
+        <div className="categoryCard" id={id}>
+            <img src={image} alt="Image" />
+            <div className="categoryName">{categoryName}</div>
+        </div>
+    );
+}
 const BodyComponent = () => {
     return (
         <div className="bodyContainer">
@@ -48,9 +101,13 @@ const BodyComponent = () => {
             </div>
             <div className="OrderNow">Order Now</div>
             <div className="foodCategoryContainer">
-                <>Food Category Components</>
+            {
+                foodCategoryList.map((item)=>(
+                     <CategoryCardComponent foodCategory={item}/>
+                    )
+                )
+            }
             </div>
-            
         </div>
     );
 }
